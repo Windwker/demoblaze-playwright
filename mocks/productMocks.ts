fd17420a2e0beba5a815b623baf982b694c0f143
Javi User <javier@MacBook-Pro-de-Javier.local>
2026-09-01 15:35:04 -0300

    Added mocks and incremented coverage in test

---
+++ mocks/productMocks.ts
@@ -0,0 +1,31 @@
+export const mockProductResponse = {
+    "Items": [
+        {
+            "cat": "phone",
+            "desc": "The Samsung Galaxy S6 is powered by 1.5GHz octa-core Samsung Exynos 7420\n processor and it comes with 3GB of RAM. The phone packs 32GB of \ninternal storage cannot be expanded. ",
+            "id": 1,
+            "img": "imgs\/galaxy_s6.jpg",
+            "price": 360,
+            "title": "Samsung galaxy s6asdasdasdasdasdasd##!@#!@$!@$!@$!@$!@$!@$!@$!@!@%ASsfasfsffsaf  fas"
+        },
+        {
+            "cat": "phone",
+            "desc": "The Samsung Galaxy S6 is powered by 1.5GHz octa-core Samsung Exynos 7420\n processor and it comes with 3GB of RAM. The phone packs 32GB of \ninternal storage cannot be expanded.The Samsung Galaxy S6 is powered by 1.5GHz octa-core Samsung Exynos 7420\n processor and it comes with 3GB of RAM. The phone packs 32GB of \ninternal storage cannot be expanded.The Samsung Galaxy S6 is powered by 1.5GHz octa-core Samsung Exynos 7420\n processor and it comes with 3GB of RAM. The phone packs 32GB of \ninternal storage cannot be expanded.The Samsung Galaxy S6 is powered by 1.5GHz octa-core Samsung Exynos 7420\n processor and it comes with 3GB of RAM. The phone packs 32GB of \ninternal storage cannot be expanded. ",
+            "id": 1,
+            "img": "imgs\/galaxy_s6.jpg",
+            "price": 360222224444124124125125125125125125125125125125125161616161616,
+            "title": "TESTING"
+        },
+        {
+            "cat": "phone",
+            "desc": "ssor and it comes with 3GB of RAM. The phonennot be expanded. ",
+            "id": 1,
+            "img": "imgs\/galaxy_s6.jpg",
+            "price": 36110,
+            "title": "Samsung galaxy s8"
+        },
+    ],
+    "LastEvaluatedKey": {
+        "id": "3"
+    }
+}
--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -2,15 +2,13 @@ import {expect, test} from "@playwright/test";
 import {HomePage} from "../pages/HomePage";
 import {HeaderWidget} from "../pages/HeaderWidget";
 import {FooterWidget} from "../pages/FooterWidget";
+import {mockProductResponse} from "../mocks/productMocks";
 
 let homePage: HomePage;
 let headerWidget: HeaderWidget;
 let footerWidget: FooterWidget;
 
-test.beforeAll(async ({browser}) => {
-    const context = await browser.newContext();
-    const page = await context.newPage();
-
+test.beforeEach(async ({page}) => {
     headerWidget = new HeaderWidget(page);
     footerWidget = new FooterWidget(page);
     homePage = new HomePage(page);
@@ -45,7 +43,7 @@ test.describe('Home page test suite', () => {
         expect(await cartPage.getProductPrice('Samsung galaxy s6')).toEqual('360');
     })
 
-    test.only('Add a product to the cart, and place the order successfully', async () => {
+    test.skip('Add a product to the cart, and place the order successfully', async () => {
         const productPage = await homePage.getProduct('Nexus 6');
         await productPage.addToCart();
         const cartPage = await headerWidget.goToCartPage();
@@ -53,7 +51,19 @@ test.describe('Home page test suite', () => {
         await cartPage.completeTheForm('Javier', 'Argentina', 'Cordoba', '2424242424242', '12', '2009');
         await cartPage.clickPurchaseButton();
         await expect(cartPage.getConfirmationAlert()).toBeVisible();
-        console.log('asd');
+    })
 
+    test.skip('Mock the response with different products, make sure all the product cards are displayed successfully', async ({page}) => {
+        await page.route('https://api.demoblaze.com/entries', async route => {
+
+            await route.fulfill({
+                status:200,
+                contentType: 'application/json',
+                body: JSON.stringify(mockProductResponse)
+            })
+        })
+
+        await page.reload();
+        await expect(page.locator('#tbodyid .card-title')).toHaveText(['Samsung galaxy s6']);
     })
 })
